c8d526da25558b175646cdaa6465ad74e6f413a7
perrine022 <perrine.honore@gmail.com>
2025-12-23 17:28:01 +0100

    add application properties, Maven wrapper, and initial test setup

---
+++ src/test/java/com/allinconnect/allinconnectback2/AllinconnectBack2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allinconnect.allinconnectback2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllinconnectBack2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
